223b1d5385c10e4079748f39c3dff56003fdfeb8
shreyanshiBarat <shreyanshi6972@ggn.hxls.org>
2026-02-17 19:43:28 +0530

    Lesson 23

--- Lesson 23/script2.js
+++ Lesson 23/script2.js
@@ -1,6 +1,6 @@
 function myFunction() {
-  document.getElementById("result").innerHTML =
+  document.getElementById("result").innerHTML = "Have a nice day"
   document.getElementById("demo").firstChild.nodeValue;
-  document.getElementById("result1").innerHTML =
+  document.getElementById("result1").innerHTML = "Happy Day!"
   document.getElementById("demo").childNodes[0].nodeValue;
 }
